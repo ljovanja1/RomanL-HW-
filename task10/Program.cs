b56c3c16f29e171d38f6a038e5e86eb4c1f00d31
roman <you@example.com>
2023-06-28 11:24:15 +0300

    end task 10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходепоказывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int randomNumber = new Random().Next(100, 1000);
+int firstDigit = randomNumber / 100;
+int secondDigit = randomNumber % 100 / 10;
+int thirdDigit = randomNumber % 10;
+Console.WriteLine($"Первая цифра: {firstDigit}; Вторая цифра {secondDigit}; третья: {thirdDigit}");
+Console.WriteLine(secondDigit);
