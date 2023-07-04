a254d0324c3546bedf8a0972cf7d0e6f1df9b2c8
roman <you@example.com>
2023-07-04 19:00:54 +0300

    end code

---
+++ task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.Write("Введиете 1 число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введиете 2 число ");
+int y = Convert.ToInt32(Console.ReadLine());
+double d = 0;
+for (int i = 1; i < 10; i++)
+{
+   d = (Math.Pow(x, y));
+   Console.WriteLine($"Число {x} в степени {y} = {d}");
+}
---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int randomNumber = new Random().Next(1, 10900);
+Console.WriteLine(randomNumber);
+    for (int i = 0; i < randomNumber; i++)
+    {
+        int firstDigit = randomNumber / 1000;
+        int secondDigit = randomNumber  % 1000 / 100;
+        int thirdDigit = randomNumber  % 100 / 10;
+        int fourthDigit = randomNumber % 10;
+        int sum = firstDigit + secondDigit + thirdDigit + fourthDigit;
+        Console.WriteLine($"{firstDigit} + {secondDigit} + {thirdDigit} + {fourthDigit} = {sum}");
+        break;
+    }
