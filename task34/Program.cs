e9c0a335e5fade9709b018bc3205a19e967013a2
roman <you@example.com>
2023-07-10 13:07:08 +0300

    end homework

---
+++ task34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int size = 10;
+int[] array = new int[size];
+
+int Negative = 0;
+
+for (int i = 0; i < size; i++) 
+{
+    array[i] = new Random().Next(100, 999);
+    if (array[i] % 2 == 0)
+    { 
+        Negative++;
+        Console.WriteLine($"Число {array[i]} - четное");
+    }
+    else
+    {
+    Console.WriteLine($"Число {array[i]} - нечетное");        
+    }
+}
+Console.WriteLine($"Четных чисел в массиве всего = {Negative}");
---
+++ task38/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+// double [] array = {3.22, 4.2, 1.15, 77.15, 65.2};
+
+int size = 12;
+int[] array = new int[size];
+
+double max = 0;
+double min = 0;
+double diff = 0;
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    if (array[i] < array[0])
+    {
+        min = array[i];
+    }
+    else if (array[i] > max)
+    {
+        max = array[i];
+    }
+    diff = max - min;
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine(diff);
